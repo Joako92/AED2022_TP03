1375a3957f29e2b7a1914003ac3685d98468ecb4
Joaquin Brondo <91130044+Joako92@users.noreply.github.com>
2022-09-01 15:37:19 -0300

    Add files via upload

---
+++ Modulo.py
@@ -0,0 +1,91 @@
+"""De cada proyecto se conoce:
+    • Número de proyecto
+    • Título
+    • Fecha de actualización con el formato dd-mm-yyyy validando que el año esté entre 2000 y 2022
+    • Lenguaje(siendo 0:Python, 1:Java, 2:C++, 3:Javascript, 4:Shell, 5:HTML, 6:Ruby, 7:Swift, 8: C#, 9:VB, 10:Go)
+    • Cantidad de líneas de código"""
+import random
+
+
+class Proyecto:
+    def __init__(self, num, tit, fech, leng, cant):  # Setter
+        self.numero = num
+        self.titulo = tit
+        self.fecha = fech  # formato dd-mm-yyyy validando que el año esté entre 2000 y 2022
+        self.lenguaje = leng  # (siendo 0:Python, 1:Java, 2:C++, 3:Javascript, 4:Shell, 5:HTML, 6:Ruby, 7:Swift, 8: C#, 9:VB, 10:Go)
+        self.cant_lineas = cant
+
+
+def to_string(proyecto):  # Esto tendria que ser un getter
+    # en lugar de mostrarse el identificador del lenguaje se debe mostrar su nombre
+    return f"{proyecto.numero:<10}{proyecto.titulo:<20}{proyecto.fecha:<15}{lenguaje(proyecto.lenguaje):<15}{proyecto.cant_lineas:<8}"
+
+
+def encabezado():
+    # Devuelve los titulos para poner sobre la funcion to_string()
+    return "{:<10}{:<20}{:<15}{:<15}{:<8}".format("N°", "Titulo", "Fecha", "Lenguaje", "Lineas")
+
+
+def lenguaje(leng):
+    lenguajes = ("Python", "Java", "C++", "Javascript", "Shell", "HTML", "Ruby", "Swift", "C#", "VB", "Go")
+    return lenguajes[leng]
+
+
+def generar_proyectos(cantidad, array):  # Esto genera los 'n' proyectos de forma aleatoria
+    for i in range(cantidad):
+        # Numero
+        flag = True
+        while flag:
+            num= random.randint(1, 1000000)
+
+            # si existe registro en el array, se comprueba que el numero no se repita
+            if len(array) > 0:
+                flag = False
+                for j in range(len(array)):
+                    if array[j].numero == num:
+                        flag = True
+
+                    else:
+                        pass
+
+                if flag:
+                    pass
+
+                else:
+                    # si existe registro, pero el numero no se repite, sale
+                    break
+
+            else:
+                # si no existe registro no comprueba nada
+                break
+
+        # Titulo
+        tit = 'Proyecto ' + str(num)
+
+        # Fecha
+        mes= random.randint(1, 12)
+
+        if mes in (1, 3, 5, 7, 8, 10, 12):
+            dia= random.randint(1, 31)
+
+        elif mes == 2:
+            dia= random.randint(1, 28)
+
+        else:
+            dia= random.randint(1, 30)
+
+        agno= random.randint(2000, 2022)
+
+        fech= str(dia) + '-' + str(mes) + '-' + str(agno)
+
+        # Lenguaje
+        leng= random.randint(0, 10)
+
+        # Cantidad Lineas
+        cant= random.randint(1, 1000000)
+
+        obj= Proyecto(num, tit, fech, leng, cant) # Crea el objeto
+
+        array.append(obj)   # Añade a la lista
+
+    return
---
+++ Principal.py
@@ -0,0 +1,181 @@
+"""1)Cargar proyectos:La generación de n proyectos de software cargados en un arreglo de registros.
+Para ello se pueden generar los datos totalmente aleatorios
+o bien contar con proyectos completos precargados e ir seleccionando aleatoriamente entre los mismos.
+Cada vez que selecciona esta opción durante la ejecución se agregarán datos al arreglo,
+sin eliminar los que ya estaban cargados. El número del proyecto no debe repetirse dentro del arreglo.
+
+2)Listar proyectos:Mostrar todos los proyectos contenidos en el arreglo,
+pero ordenados alfabéticamente por título.
+Cada proyecto debe ocupar un máximo de dos líneas en pantalla
+y en lugar de mostrarse el identificador del lenguaje se debe mostrar su nombre.
+
+3)Actualizar proyecto: Buscar si existe un proyecto con número x,
+siendo x un valor que se ingrese por teclado.
+Si existe, se debe permitir modificar su cantidad de líneas de código
+y la fecha de actualización (recuerde que debe cumplir con el formato dd-mm-yyyy).
+Si no existe, debe indicar con un mensaje.
+
+4)Resumen por lenguaje: Calcular la cantidad de líneas de código acumuladas por lenguaje.
+Mostrar los resultados teniendo en cuenta que se debe visualizar el nombre del lenguaje en lugar del código.
+
+5)Resumen por año: Calcular la cantidad de proyectos por año de actualización,
+considerando los años entre 2000 y 2022 inlcuidos ambos.
+Mostrar los resultados sólo de los años que tengan algún proyecto de software.
+
+6)Filtrar lenguaje: Mostrar los proyectos de software ordenados por número de proyecto de manera ascendente,
+del lenguaje ln, siendo ln un valor ingresado por teclado.
+
+7)Productividad: A partir del resultado obtenido en el punto 5,
+determinar el año con mayor cantidad de proyectos actualizados,
+considerando mostrar todos los años si fuera más de uno con dicha cantidad."""
+import random
+import Modulo
+
+
+def validar():
+    num = int(input("Ingrese un numero mayor a 0: "))
+    while num < 1:
+        print("Error...")
+        num = int(input("Ingrese un numero mayor a 0: "))
+    return num
+
+
+def validar_entre(inf, sup):
+    num = int(input(f"Ingrese un numero entre {inf} y {sup}: "))
+    while inf > num or num > sup:
+        print("Error...")
+        num = int(input(f"Ingrese un numero entre {inf} y {sup}: "))
+    return num
+
+
+def cargar_fecha():
+    dia = int(input("Ingrese dia (DD): "))
+    mes = int(input("Ingrese mes (MM): "))
+    anio = int(input("Ingrese año (AAAA): "))
+    return f"{dia:<2}/{mes:<2}/{anio:<4}"
+
+
+def generar_titulo():
+    letras = ("A", "B", "C", "D", "E")
+    return f"{random.choice(letras)}{random.choice(letras)}{random.choice(letras)}{random.randint(100, 999)}"
+
+
+def carga_manual(v_proy):
+    n = len(v_proy)
+    for i in range(n):
+        numero = int(input("Ingrese numero para el proyecto: "))
+        titulo = input("Ingrese titulo del proyecto: ")
+        fecha = cargar_fecha()
+        leng = input("Ingrese codigo del lenguaje (siendo 0:Python, 1:Java, 2:C++, 3:Javascript, 4:Shell, 5:HTML, 6:Ruby, 7:Swift, 8: C#, 9:VB, 10:Go): ")
+        cant_lineas = int(input("Ingrese cantidad de lineas del codigo: "))
+        v_proy[i] = Modulo.Proyecto(numero, titulo, fecha, leng, cant_lineas)
+
+
+def carga_auto(v_proy):
+    n = len(v_proy)
+    for i in range(n):
+        numero = random.randint(1000, 9999)
+        titulo = generar_titulo()
+        fecha = f"{random.randint(10, 30)}/{random.randint(1, 12)}/{random.randint(2000, 2022)}"
+        leng = random.randint(0, 10)
+        cant_lineas = random.randint(100, 999)
+        v_proy[i] = Modulo.Proyecto(numero, titulo, fecha, leng, cant_lineas)
+
+
+def listar(v_proy):
+    n = len(v_proy)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if v_proy[i].titulo > v_proy[j].titulo:
+                v_proy[i], v_proy[j] = v_proy[j], v_proy[i]
+
+    print(Modulo.encabezado())
+    for i in range(n):
+        print(Modulo.to_string(v_proy[i]))
+
+
+def opcion5():
+    pass
+
+
+def opcion6():
+    pass
+
+
+def opcion7():
+    pass
+
+
+def menu():
+    # Iniciar el vector que contiene los proyectos
+    v_proyectos = [None]
+    op = 0
+    while op != 8:
+        print("\nMENU DE OPCIONES")
+        print("-" * 50)
+        print('1. Cargar proyectos')  # Debe permitir la carga manual o automatica
+        print('2. Listar proyectos')
+        print('3. Actualizar proyecto')
+        print('4. Resumen por lenguaje')
+        print('5. Resumen por año')
+        print('6. Filtrar lenguaje')
+        print('7. Productividad')
+        print('8. Salir')
+        print("-" * 50)
+        print('Ingrese opcion...')
+        op = validar_entre(1, 8)
+
+        if op == 1:
+            print("Ingrese la cantidad de proyectos que desea controlar...")
+            cant = validar()
+            if v_proyectos[0] is None:
+                cant = cant - 1
+                proyectos = [None] * cant
+                v_proyectos.extend(proyectos)
+            else:
+                proyectos = [None] * cant
+                v_proyectos.extend(proyectos)
+            print("Ingrese 1 para carga manual - 2 para carga automatica...")
+            elec = validar_entre(1, 2)
+            if elec == 1:
+                carga_manual(v_proyectos)
+            if elec == 2:
+                carga_auto(v_proyectos)
+            print("---Carga completa---")
+            input("Pulse enter para continuar...")
+
+        else:
+            if v_proyectos[0] is None:
+                print("Aun no se han cargado proyectos...")
+
+            else:
+                if op == 2:
+                    listar(v_proyectos)
+                    input("Pulse enter para continuar...")
+
+                elif op == 3:
+                    opcion3()
+                    input("Pulse enter para continuar...")
+
+                elif op == 4:
+                    opcion4()
+                    input("Pulse enter para continuar...")
+
+                elif op == 5:
+                    opcion5()
+                    input("Pulse enter para continuar...")
+
+                elif op == 6:
+                    opcion6()
+                    input("Pulse enter para continuar...")
+
+                elif op == 7:
+                    opcion7()
+                    input("Pulse enter para continuar...")
+
+        if op == 8:
+            print("--- Programa finalizado ---")
+
+
+if __name__ == '__main__':
+    menu()
